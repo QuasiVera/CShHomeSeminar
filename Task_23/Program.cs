e7bf94a7d5c1e98bc438b3a5738ac9f46e14a91d
VeraBu <opilki-in-yhe-head@mail.ru>
2022-11-21 23:30:24 +0300

    added Task 23

---
+++ Task_23/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 23
+// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+int N = UserCoord("Введите число N: ");
+
+for(int i = 1; i<= N; i++)
+{
+    Console.WriteLine($"{i}^3 = {i*i*i}");
+}
+
+
+
+//----------------Ввод данных
+static int UserCoord(string messageToUser)
+{
+    Console.Write(messageToUser);
+    try
+    {
+        return int.Parse(Console.ReadLine() ?? "");
+    }
+    catch (Exception exc)
+    {
+        Console.WriteLine($"Ошибка ввода данных! {exc.Message}");
+        return 0;
+    }
+}
