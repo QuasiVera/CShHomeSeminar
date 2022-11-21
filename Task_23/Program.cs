46d2fee6016b003e8cb9bca2f55288855e528871
VeraBu <opilki-in-yhe-head@mail.ru>
2022-11-21 23:40:51 +0300

    corrected Task 23

--- Task_23/Program.cs
+++ Task_23/Program.cs
@@ -2,13 +2,17 @@
 // Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
 
 int N = UserCoord("Введите число N: ");
+MethodCube(N);
 
-for(int i = 1; i<= N; i++)
+//---------------------------------------Методы---------------------
+//-------------Таблица кубов
+void MethodCube(int Number)
 {
+    for(int i = 1; i<= Number; i++)
+    {
     Console.WriteLine($"{i}^3 = {i*i*i}");
+    }
 }
-
-
 
 //----------------Ввод данных
 static int UserCoord(string messageToUser)
